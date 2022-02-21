9a68d22a98658254d86ac044ee9a9af4a32cd19c
Wendelin Gerein <gereinw@gmail.com>
2022-02-21 09:59:04 +0100

    Initial commit

---
+++ pages/_test/plugin/markdownit/file2.vue
@@ -0,0 +1,13 @@
+<template lang="md" src="~/assets/_test/plugin/markdownit/hello.md">
+
+</template>
+
+<script lang="ts">
+export default {
+	name: "file2",
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
